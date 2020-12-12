0428ab0ba272d9d8776c61d1c0d121ed99e40590
James <jmw1982@gmail.com>
2020-12-12 20:41:26 +1100

    Revert "moving away from setup.py"
    This reverts commit 49f42bb8d4dd98e1b31287301efe64e9d94ee584.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="generic-ipsum",
+    version="0.0.1",
+    author="James Wakefield",
+    author_email="jmw1982@gmail.com",
+    description="Generic lorem ipsum text generating class",
+    url="https://github.com/jwakefieldau/generic-ipsum",
+    packages=setuptools.find_packages(),
+)

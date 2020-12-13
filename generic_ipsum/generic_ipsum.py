d8812452eef4ab4258b20593661d22e71bc617b2
James <jmw1982@gmail.com>
2020-12-13 16:49:34 +1100

    keyword directed text, remove pointless support for literal lists

--- generic_ipsum/generic_ipsum.py
+++ generic_ipsum/generic_ipsum.py
@@ -1,6 +1,8 @@
 import random
 import re
 
+from collections import defaultdict
+
 # generic template processing, separate this into another module
 class GenericIpsum(object):
 
@@ -9,50 +11,74 @@ class GenericIpsum(object):
 
         # change these - might not need to be res
         self.num_range_re = re.compile(r'([0-9]+)-([0-9]+)')
-        self.literal_list_re = re.compile(r"('[^']+',\s*)+'[^']+'")
-        self.literal_list_split_re = re.compile(r'\W+')
+        self.kw_map = self.build_kw_map()
+        self.tag_frag_map = self.build_tag_frag_map()
 
+    def build_kw_map(self):
+        kw_map = defaultdict(list)
+        for (tag_name, tag_kw_list,) in self.terms.items():
+            for tag_kw in tag_kw_list:
+                kw_map[tag_kw].append(tag_name)
+        return kw_map
+
+    def build_tag_frag_map(self):
+        tag_frag_map = defaultdict(list)
+        for (i, frag_list,) in enumerate(self.fragments):
+            for (j, frag,) in enumerate(frag_list):
+                for tag in self.tag_re.findall(frag):
+                    tag_frag_map[tag].append((i,j,))
+        return tag_frag_map
+                
     def num_range(self, min_num, max_num):
         num_list = list(range(min_num, max_num + 1))
         return str(random.choice(num_list))
 
     def literal_list(self, list_str):
-        split_elems = re.split(self.literal_list_split_re, list_str)
-        literal_elem_list = [elem for elem in split_elems if elem != '']
+        literal_elem_list = self.literal_list_re.findall(list_str)
         return random.choice(literal_elem_list)
 
     def term(self, term_str):
         term_choice_list = self.terms[term_str]
-
         return random.choice(term_choice_list)
 
-    def process_tag(self, m):
-        inner_str = m.group(1)
-        
+    def process_tag(self, inner_str):
         inner_m = self.num_range_re.match(inner_str)
         if inner_m:
             return self.num_range(int(inner_m.group(1)), int(inner_m.group(2)))
- 
-        inner_m = self.literal_list_re.match(inner_str)
-        if inner_m:
-            return self.literal_list(inner_str)
-
         else:
             return self.term(inner_str)
         
-    def process_fragment(self, frag_str):
-        return self.tag_re.sub(self.process_tag, frag_str)
+    def process_fragment(self, frag_str, keyword=None, kw_tag=None):
+        #return self.tag_re.sub(self.process_tag, frag_str)
+        for inner_tag_str in self.tag_re.findall(frag_str):
+            if keyword and kw_tag == inner_tag_str:
+                frag_str = frag_str.replace(f"[{inner_tag_str}]", keyword, 1)
+            else:
+                frag_str = frag_str.replace(f"[{inner_tag_str}]", self.process_tag(inner_tag_str), 1)
+        return frag_str
 
-    def get(self):
+    def get(self, keyword=None):
         ret_str_list = []
 
-        for cur_frag_list in self.fragments:
-            cur_frag = random.choice(cur_frag_list)
-            proc_frag = self.process_fragment(cur_frag)
+        if keyword:
+            kw_tag_list = self.kw_map.get(keyword)
+            if not kw_tag_list:
+                return None
+            tag_choice = random.choice(kw_tag_list)
+            tag_frag_list = self.tag_frag_map.get(tag_choice)
+            (frag_choice_i, frag_choice_j,) = random.choice(tag_frag_list)
+
+        for (i, cur_frag_list,) in enumerate(self.fragments):
+            if keyword and i == frag_choice_i:
+                cur_frag = cur_frag_list[frag_choice_j]
+                proc_frag = self.process_fragment(cur_frag, keyword=keyword, kw_tag=tag_choice)
+            else:     
+                cur_frag = random.choice(cur_frag_list)
+                proc_frag = self.process_fragment(cur_frag)
             ret_str_list.append(proc_frag)
 
         return '. '.join(ret_str_list)
- 
+
     def __iter__(self):
         return self
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="generic-ipsum",
-    version="0.0.1",
+    version="0.0.2",
     author="James Wakefield",
     author_email="jmw1982@gmail.com",
     description="Generic lorem ipsum text generating class",
